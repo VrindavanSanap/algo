10d9f880b93f1ba8dc750cd18f2fe25b3b43e929
Vrindavan Sanap <vrindavansanap@gmail.com>
2024-08-13 03:36:44 +0530

    0

---
+++ leetcode/strings/goal_parser.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+char* interpret(char* command){
+  int i = 0;
+  while(i < strlen(command) - 2  ){
+    char c1 = command[i];
+    char c2 = command[i + 1];
+    if (c1 == 'G'){
+      i += 1;
+    }else if (c1 == '(' && c2 == ')'){
+      printf("%c", c1);
+      i += 2;
+    }else if (c1 == '(' && c2 == 'a'){
+      printf("al");
+      i += 2;
+    }
+    else{
+
+      i += 1;
+    }
+  }
+}
+
+int main(){
+  char* c1 = "G()(al)";
+  char* c2 = "G()()()()(al)";
+  interpret(c1); 
+//  interpret(c2); 
+  return 0;
+}
---
+++ leetcode/strings/permutation_diff.c
@@ -0,0 +1,41 @@
+// Created by Vrindavan Sanap
+// Solves https://leetcode.com/problems/permutation-difference-between-two-strings/description/
+// 
+//
+//
+//
+//
+//
+//
+//
+//
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+
+int findPermutationDifference(char* s, char* t) {
+  int total_diff = 0;
+  for (int i = 0; i< (int)strlen(s);i++){
+    char char_s = s[i];
+    char char_t = t[i];
+    int diff_i = abs(i - (strchr(s, char_t) - s)); 
+    total_diff += diff_i;
+  }
+  return total_diff;
+}
+
+int main(){
+
+  char* s1 = "abc";
+  char* t1 = "bac";
+  int total_diff = findPermutationDifference(s1, t1);
+  printf("%d", total_diff);
+  char* s2 = "abcde";
+  char* t2 = "edbac";
+  int total_diff2 = findPermutationDifference(s2, t2);
+  printf("%d", total_diff2);
+ 
+  return 0;
+}
