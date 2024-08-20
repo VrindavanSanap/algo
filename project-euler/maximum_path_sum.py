a84bd871aa652cada6f0a62814d4f487c732c122
Vrindavan Sanap <vrindavansanap@gmail.com>
2024-08-20 17:37:09 +0530

    =

--- project-euler/maximum_path_sum.py
+++ project-euler/maximum_path_sum.py
@@ -1,18 +1,9 @@
 import numpy as np
 
-with open("triangle.txt") as f:
-  data = f.read().strip().split("\n")
+    with open("triangle.txt") as f:data = f.read().strip().split("\n")
 
-data_matrix = []
-for line in data:
-  data_matrix.append(np.array([int(i) for i in line.split()]))
+                                                             data_matrix =[] for line in data:data_matrix.append(np.array([int(i) for i in line.split()]))
 
+                                                                                                                     def get_max_neighbours(row) :print(row) return np.maximum(row[ : - 1], row[1 : ])
 
-def get_max_neighbours(row):
-  print(row)
-  return np.maximum(row[:-1], row[1:])
-
-
-prev = np.zeros(len(data_matrix[-1]))
-for row in reversed(data_matrix):
-  prev = get_max_neighbours(row + prev)
+                                                                                                                                                                                                prev = np.zeros(len(data_matrix[- 1])) for row in reversed(data_matrix) :prev = get_max_neighbours(row + prev)
--- project-euler/square_digit_chain.c
+++ project-euler/square_digit_chain.c
@@ -1,40 +1,36 @@
-#include <stdio.h>
 #include <math.h>
+#include <stdio.h>
 
-int sum_square_digits(int n){
+int sum_square_digits(int n) {
   int sum_squares = 0;
-  while(n!=0){
-    sum_squares += pow((n%10),2) ;
+  while (n != 0) {
+    sum_squares += pow((n % 10), 2);
     n /= 10;
   }
   return sum_squares;
 }
 
-
-int digit_chain_recursive(int n){
- if (n == 0 || n == 1 || n == 89){
+int digit_chain_recursive(int n) {
+  if (n == 0 || n == 1 || n == 89) {
     return n;
-  }else{
+  } else {
     return digit_chain_recursive(sum_square_digits(n));
   }
 }
 
-
-int main(){
-  int ends_with_1 = 0; 
-  int ends_with_89 = 0; 
-  for (int i = 0; i < 10e7; i++ ){
-    if (i%1000000== 0){
+int main() {
+  int ends_with_1 = 0;
+  int ends_with_89 = 0;
+  for (int i = 0; i < 10e7; i++) {
+    if (i % 1000000 == 0) {
       printf("%.2f %% done \n", (i / 10e7) * 100);
     }
-    int ans =  digit_chain_recursive(i);
-    if (ans == 1){
+    int ans = digit_chain_recursive(i);
+    if (ans == 1) {
       ends_with_1 += 1;
-    }else if (ans == 89){
+    } else if (ans == 89) {
       ends_with_89 += 1;
     }
-
   }
- printf("%d -  %d", ends_with_1, ends_with_89);
-
+  printf("%d -  %d", ends_with_1, ends_with_89);
 }
