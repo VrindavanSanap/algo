9ef9e31984edbc89e9227e9b4c2b2d9491165ba1
Vrindavan Sanap <vrindavansanap@gmail.com>
2024-08-20 23:35:35 +0530

    -

--- searching/binary.py
+++ searching/binary.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 import math
+from typing import List
 
-data = list(range(1, 11))
+data: List[int] = list(range(1, 11))
 
 
-def binary_search(data, value):
-  l = 0
-  r = len(data) - 1
+def binary_search(data: List[int], value: int) -> int:
+  l: int = 0
+  r: int = len(data) - 1
   while l <= r:
-    m = math.floor((l + r) / 2)
-    value_m = data[m]
+    m: int = math.floor((l + r) / 2)
+    value_m: int = data[m]
     print(value_m)
     if value_m == value:
       return m
@@ -21,5 +22,5 @@ def binary_search(data, value):
   return -1
 
 
-res = binary_search(data, 9)
+res: int = binary_search(data, 9)
 print(f"found at index:{res}")
